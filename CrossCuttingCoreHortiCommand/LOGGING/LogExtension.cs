5c781518291c2368d40ea1aec22515f7cdf7ef00
dnacbar <douglasnac1@hotmail.com>
2020-02-05 21:06:12 -0300

    HORTI - CREATING LOG PT-02

---
+++ CrossCuttingCoreHortiCommand/LOGGING/LogExtension.cs
@@ -0,0 +1,56 @@
+﻿using Serilog;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace CrossCuttingCoreHortiCommand.LOG
+{
+    public static class LogExtension
+    {
+        public static void CreateLog(LogObject logMongoObject)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                switch (logMongoObject.LevelLog)
+                {
+                    case ENUM.EnumLevelLog.Error:
+                        Log.Logger = new LoggerConfiguration()
+                                         .WriteTo
+                                         .File(@"C:\log\log_error.txt", rollingInterval: RollingInterval.Hour)
+                                         .CreateLogger();
+                        break;
+                    case ENUM.EnumLevelLog.Fatal:
+                        Log.Logger = new LoggerConfiguration()
+                                         .WriteTo
+                                         .File(@"C:\log\log_fatal.txt", rollingInterval: RollingInterval.Hour)
+                                         .CreateLogger();
+                        break;
+                    case ENUM.EnumLevelLog.Information:
+                        Log.Logger = new LoggerConfiguration()
+                                         .WriteTo
+                                         .File(@"C:\log\log_information.txt", rollingInterval: RollingInterval.Hour)
+                                         .CreateLogger();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Log.Logger = new LoggerConfiguration()
+                                 .WriteTo
+                                 .File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs/.log"), rollingInterval: RollingInterval.Day)
+                                 .CreateLogger();
+            }
+
+            if (logMongoObject.LevelLog == ENUM.EnumLevelLog.Information)
+                Log.Information(logMongoObject.LogMessage());
+
+            else if (logMongoObject.LevelLog == ENUM.EnumLevelLog.Error)
+                Log.Error(logMongoObject.LogMessage());
+
+            else if (logMongoObject.LevelLog == ENUM.EnumLevelLog.Fatal)
+                Log.Fatal(logMongoObject.LogMessage());
+        }
+    }
+}
---
+++ CrossCuttingCoreHortiCommand/LOGGING/LogMongoObject.cs
@@ -0,0 +1,23 @@
+﻿using CrossCuttingCoreHortiCommand.ENUM;
+using System;
+using System.Net;
+
+namespace CrossCuttingCoreHortiCommand.LOG
+{
+    public class LogObject
+    {
+        public string Id { get; set; }
+        public string UserLog { get; set; }
+        public string InfoLog { get; set; }
+        public EnumLevelLog LevelLog { get; set; }
+        public DateTime TimeLog { get; set; }
+        public IPAddress IPAddress { get; set; }
+
+        public string LogMessage()
+        {
+            return @"DATETIME: " + TimeLog + " ID: " + Id + " USER: " + UserLog + " \r\n" +
+                    "IP: " + IPAddress.MapToIPv4().ToString() + " \r\n" +
+                    "LOG: " + InfoLog;
+        }
+    }
+}
