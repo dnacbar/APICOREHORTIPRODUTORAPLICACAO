a99f67b66c745f7ac322d20de7c9c362c8d509bd
dnacbar <douglasnac1@hotmail.com>
2020-02-05 21:29:49 -0300

    HORTI - CREATING LOG PT-03

---
+++ WebApiCoreHortiQuery/LOGGING/LogExceptionMiddleware.cs
@@ -0,0 +1,79 @@
+﻿using CrossCuttingCoreHortiCommand.EXCEPTION;
+using CrossCuttingCoreHortiCommand.LOG;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WebApiCoreHortiQuery
+{
+    public class LogExceptionMiddleware
+    {
+        private readonly RequestDelegate requestDelegate;
+
+        public LogExceptionMiddleware(RequestDelegate _requestDelegate)
+        {
+            requestDelegate = _requestDelegate ?? throw new ArgumentNullException(nameof(requestDelegate));
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await requestDelegate(httpContext);
+            }
+            catch (HttpStatusCodeException ex)
+            {
+                if (ex.StatusCode == HttpStatusCode.InternalServerError)
+                {
+                    LogExtension.CreateLog(new LogObject
+                    {
+                        Id = new Guid().ToString(),
+                        UserLog = "",
+                        InfoLog = ex.ToString(),
+                        LevelLog = CrossCuttingCoreHortiCommand.ENUM.EnumLevelLog.Fatal,
+                        TimeLog = DateTime.Now,
+                        IPAddress = httpContext.Connection.RemoteIpAddress
+                    });
+                }
+                else if (ex.StatusCode == HttpStatusCode.BadRequest)
+                {
+                    LogExtension.CreateLog(new LogObject
+                    {
+                        Id = new Guid().ToString(),
+                        UserLog = "",
+                        InfoLog = ex.ToString(),
+                        LevelLog = CrossCuttingCoreHortiCommand.ENUM.EnumLevelLog.Error,
+                        TimeLog = DateTime.Now,
+                        IPAddress = httpContext.Connection.RemoteIpAddress
+                    });
+                }
+                else
+                {
+                    LogExtension.CreateLog(new LogObject
+                    {
+                        Id = new Guid().ToString(),
+                        UserLog = "",
+                        InfoLog = ex.ToString(),
+                        LevelLog = CrossCuttingCoreHortiCommand.ENUM.EnumLevelLog.Information,
+                        TimeLog = DateTime.Now,
+                        IPAddress = httpContext.Connection.RemoteIpAddress
+                    });
+                }
+
+                httpContext.Response.StatusCode = (int)ex.StatusCode;
+                httpContext.Response.ContentType = ex.ContentType;
+                await httpContext.Response.WriteAsync(ex.Message);
+            }
+        }
+    }
+
+    public static class LogExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLogExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LogExceptionMiddleware>();
+        }
+    }
+}
